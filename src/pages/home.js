ce15a74dca6e9d2eb98e99267a4ed5638a87b07c
ExosamaUnion <exosamaunion@gmail.com>
2023-03-24 10:50:09 -0300

    first

---
+++ src/pages/home.js
@@ -0,0 +1,8 @@
+export default function Home () { return (
+    <div className = 'home'>
+        <p>
+        <a href = "https://opensea.io/collection/exosama-expect-chaos">BUY AN EXO</a>
+        </p>
+    </div>
+)
+}
