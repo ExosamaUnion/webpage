aa98ed3ab4aab208d19f310adec57f8a8e5755bc
ExosamaUnion <exosamaunion.com>
2023-03-26 09:58:12 -0300

    first

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,5 +1,6 @@
 export default function Footer () { return (
     <div className = "container">
+        est 24/3/2023
     </div>
 )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,6 @@ export default function Home () { return (
         <Roles/>
         <VoteLog/>
         <VoterLog/>
-        <Footer/>
     </div>
 )
 }
