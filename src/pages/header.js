d06b3f5fc5c170d8342f1ee180d5656366423f66
ExosamaUnion <exosamaunion.com>
2023-03-26 10:05:39 -0300

    first

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,12 +1,11 @@
 export default function Header () { return (
     <div className = "container">
-        <br></br>
-        <img src="exounionlogo.png" alt="exounionlogo.png" height = "20%"></img><br></br>
         <audio autoplay controls>
-<source src="music.mp3" type="audio/mpeg"></source>
-    </audio>
-    <br></br>
-    <br></br>
+            <source src="music.mp3" type="audio/mpeg"></source>
+        </audio>
+        <br/>
+        <img src="exounionlogo.png" alt="exounionlogo.png" height = "20%"></img>
+        <br/><br/>
     </div>
 )
 }
