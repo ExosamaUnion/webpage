469b9fc46f39e33daa59fc5099c3ae64757cbec4
ExosamaUnion <exosamaunion.com>
2023-03-26 10:03:31 -0300

    first

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,6 @@ import ActionList from "./actionList"
 import Roles from "./roles"
 import VoteLog from "./voteLog"
 import VoterLog from "./voterLog"
-import Footer from "./footer"
 
 export default function Home () { return (
     <div className = "home">
